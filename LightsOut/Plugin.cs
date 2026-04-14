a7218701e3cd794422317d6d565069975cf763b6
ivan tkachenko <ratijas.tkachenko@gmail.com>
2026-04-14 04:28:09 +0300

    GeneralImprovements: Remove unused member flag
    Without declaring a BepInDependency with
    BepInDependency.DependencyFlags.SoftDependency flag, it's not even
    correct to check for the GUID in chainloader.

--- LightsOut/Plugin.cs
+++ LightsOut/Plugin.cs
@@ -10,22 +10,11 @@ namespace LightsOut
         internal static ManualLogSource logger;
         private readonly Harmony harmony = new("LightsOut");
 
-        internal static bool isGIPresent = false;
-
         private void Awake()
         {
             logger = Logger;
 
             harmony.PatchAll();
-
-            if (
-                BepInEx.Bootstrap.Chainloader.PluginInfos.ContainsKey(
-                    "ShaosilGaming.GeneralImprovements"
-                )
-            )
-            {
-                isGIPresent = true;
-            }
 
             // Plugin startup logic
             Logger.LogInfo($"Plugin {MyPluginInfo.PLUGIN_GUID} is loaded!");
